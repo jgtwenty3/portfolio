96f94b7aa84a0790077c91f420cad8a63a9ff35c
Justin <justinguerrerowork@gmail.com>
2024-07-13 16:13:09 -0400

    added sentry

--- data/index.ts
+++ data/index.ts
@@ -74,9 +74,9 @@ export const navItems = [
       id: 1,
       title: "Swish Social",
       des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
+      img: "/swish.png",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "swishsocial.com",
+      link: "swishsocial.club",
     },
     {
       id: 2,
